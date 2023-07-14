54b96b66e2e49677c41760ff46ea5fff4b5cb95b
Rei333 <erika.rouere@gmail.com>
2023-07-14 17:10:45 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <link rel="stylesheet" href="common.css">
+    </head>
+
+    <body>
+        <h1>coucou</h1>
+        <p>ceci est un paragraphe nul</p>
+        <div>div vide</div>
+
+        <div><?php echo "hgf" ?></div>
+    </body>
+</html>
