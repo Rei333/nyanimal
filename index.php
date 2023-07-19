66fa929825920976fdf189f8fa6d535a80d7bf8f
Rei333 <erika.rouere@gmail.com>
2023-07-19 20:47:36 +0200

    WIP home page

--- index.php
+++ index.php
@@ -23,6 +23,38 @@
                     <a href="">S'inscrire</a>
                 </div>
             </section>
+
+            <section id="presentation">
+                <h1>Découvrez Nyanimal en quelques images :</h1>
+                <div>
+                    <img class="left" src="img/logo.png" alt="img1">
+                    <div class="right">
+                        <h2>Prends soin de ton Nyanimal</h2>
+                        Joue avec lui, veille sur lui, gagne de l'argent et décore ta maison !
+                        Plus ton Nyanimal est heureux, plus tu gagnes d'argent, plus ta maison est belle !
+                    </div>
+
+                    <div class="left">
+                        <h2>Habille ton Nyanimal</h2>
+                        Achète à la boutique des habits pour pouvoir personnaliser ton Tamagochi !
+                        Texte texte texte texte
+                    </div>
+                    <img class="right" src="img/logo.png" alt="img1">
+
+                    <img class="left" src="img/logo.png" alt="img1">
+                    <div class="right">
+                        <h2>Joue avec ton Nyanimal</h2>
+                        Texte texte texte texte texte texte texte texte texte texte texte texte texte texte
+                    </div>
+                    <div>
+                        Prêt(e) pour l'aventure ?
+                        <div>
+                            <button><a href="">Connexion</a></button>
+                            <button><a href="">Inscription</a></button>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
 
         <?php include "footer.php" ?>
