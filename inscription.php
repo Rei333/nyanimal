5760b5648df88c507bbb3ab54c9ab0035790db89
Rei <petitneko333@gmail.com>
2023-07-19 22:37:19 +0200

    wip

---
+++ inscription.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/common.css">
+        <link rel="stylesheet" href="css/inscription.css">
+    </head>
+
+    <body>
+        <?php include "header.php" ?>
+
+        <main>
+            
+        </main>
+
+        <?php include "footer.php" ?>
+    </body>
+</html>
