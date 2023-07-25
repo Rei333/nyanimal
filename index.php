bbac8d925c574e85a3a24a74a858b4a665f75c64
Rei <petitneko333@gmail.com>
2023-07-25 08:08:32 +0200

    wip inscription

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>Home</title>
+        <title>Jeu</title>
         <link rel="stylesheet" href="css/common.css">
         <link rel="stylesheet" href="css/index.css">
     </head>
@@ -10,51 +10,8 @@
     <body>
         <?php include "header.php" ?>
 
-        <main>
-            <section id="identification">
-                <h1>Connexion</h1>
-                <form action="" method="post">
-                    <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
-                    <input type="password" name="password" id="password" placeholder="Mot de passe">
-                    <input type="submit" value="Connexion">
-                </form>
-                <div>
-                    <a href="">Mot de passe oublié</a>
-                    <a href="">S'inscrire</a>
-                </div>
-            </section>
-
-            <section id="presentation">
-                <h2>Découvrez Nyanimal en quelques images :</h2>
-                <div>
-                    <img class="left" src="img/logo.png" alt="img1">
-                    <div class="right">
-                        <h2>Prends soin de ton Nyanimal</h2>
-                        Joue avec lui, veille sur lui, gagne de l'argent et décore ta maison !
-                        Plus ton Nyanimal est heureux, plus tu gagnes d'argent, plus ta maison est belle !
-                    </div>
-
-                    <div class="left">
-                        <h2>Habille ton Nyanimal</h2>
-                        Achète à la boutique des habits pour pouvoir personnaliser ton Tamagochi !
-                        Texte texte texte texte
-                    </div>
-                    <img class="right" src="img/logo.png" alt="img1">
-
-                    <img class="left" src="img/logo.png" alt="img1">
-                    <div class="right">
-                        <h2>Joue avec ton Nyanimal</h2>
-                        Texte texte texte texte texte texte texte texte texte texte texte texte texte texte
-                    </div>
-                    <div>
-                        <h3>Prêt(e) pour l'aventure ?</h3>
-                        <div id="buttons">
-                            <button><a href="">Connexion</a></button>
-                            <button><a href="">Inscription</a></button>
-                        </div>
-                    </div>
-                </div>
-            </section>
+        <main id="game">
+           
         </main>
 
         <?php include "footer.php" ?>
--- inscription.php
+++ inscription.php
@@ -2,7 +2,7 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title>Home</title>
+        <title>Inscription</title>
         <link rel="stylesheet" href="css/common.css">
         <link rel="stylesheet" href="css/inscription.css">
     </head>
@@ -11,7 +11,25 @@
         <?php include "header.php" ?>
 
         <main>
-            
+            <section>
+                <h1>Inscription</h1>
+                <form action="" method="post">
+                    <label class="col1 row1" for="pseudo">Pseudo :</label>
+                    <input class="col1 row1" type="text" name="pseudo" id="pseudo" placeholder="Pseudo">
+                    <label class="col2 row1" for="mail">E-mail :</label>
+                    <input class="col2 row1" type="email" name="mail" id="mail" placeholder="Pseudo">
+
+                    <label class="col1 row2" for="password">Mot de passe :</label>
+                    <input class="col1 row2" type="password" name="password" id="password" placeholder="Mot de passe">
+                    <input class="col2 row2" type="checkbox" id="cgu" name="cgu">
+                    <label class="col2 row2" for="cgu">J'ai lu et j'accepte les <a href="">CGU</a></label>
+                    
+                    <input type="submit" value="Inscription">
+                </form>
+                <div>
+                    <a href="/connexion">J'ai déjà un compte</a>
+                </div>
+            </section>
         </main>
 
         <?php include "footer.php" ?>
